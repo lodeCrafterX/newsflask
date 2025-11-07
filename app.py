b250d77115b289513bb0ed27791a106754084817
lodeCrafterX <marielamb0143@gmail.com>
2025-11-07 18:10:11 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 from fetch_nasa import get_nasa_news
 
-app = Flask(name)
+app = Flask(_name_)
 
 @app.route('/')
 def home():
     news = get_nasa_news()
     return render_template('index.html', news=news)
 
-if name == "main":
+if _name_ == "main":
     app.run(host='0.0.0.0', port=5000)
