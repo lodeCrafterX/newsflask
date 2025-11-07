84f17855f5a146c9d7daae01ef5a8569ee549250
lodeCrafterX <marielamb0143@gmail.com>
2025-11-07 18:14:33 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
 from fetch_nasa import get_nasa_news
+from ai_summarizer import summarize_data
+from db_save import save_to_db
 
-app = Flask(_name_)
+app = Flask(name)
 
 @app.route('/')
 def home():
-    news = get_nasa_news()
-    return render_template('index.html', news=news)
+    data = get_nasa_news()
+    summary = summarize_data(data)
+    save_to_db(summary)
+    return render_template('index.html', news=summary)
 
-if _name_ == "main":
+if name == "main":
     app.run(host='0.0.0.0', port=5000)
